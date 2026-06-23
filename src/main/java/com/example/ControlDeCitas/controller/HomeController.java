e0623ec7890aae882a8f2f58639e3bce91d21e3d
DavidJesusMontes <i202502777@cibertec.edu.pe>
2026-06-22 22:49:37 -0500

    proyecto finalizado

--- src/main/java/com/example/ControlDeCitas/controller/HomeController.java
+++ src/main/java/com/example/ControlDeCitas/controller/HomeController.java
@@ -10,9 +10,7 @@ public class HomeController {
     @GetMapping({"/", "/home"})
     public String home(Model model) {
         model.addAttribute("content", "home");
-        // Because home uses menu and footer directly, we don't need layout if it's already structured that way
-        // But the user said "adapta el proyecto para hacer uso de los archivos html como errorLogin , footer, home , layout , login, menu, msgError"
-        // So we just return "home" which will process home.html and include menu and footer.
+
         return "home";
     }
 
